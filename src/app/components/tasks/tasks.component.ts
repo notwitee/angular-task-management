f50fa5bd79218f51784b47fc610023ceebad881d
notwitee <marchel.code@gmail.com>
2025-10-01 17:53:16 +0700

    feat: modal for create new task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,18 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../../data/dummy-tasks';
+import { newTask, TaskModalComponent } from './task-modal/task-modal.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, TaskModalComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   public readonly userId = input.required<string>();
   public readonly userName = input.required<string>();
-
+  public readonly addTask = signal<boolean>(false);
   public tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
@@ -21,5 +22,16 @@ export class TasksComponent {
 
   public onCompletedTaskId(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  public createNewTask(task: newTask) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId(),
+      title: task.title,
+      summary: task.summary,
+      dueDate: task.dueDate
+    });
+    this.addTask.set(false);
   }
 }
