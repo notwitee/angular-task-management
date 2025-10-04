411b45d2d39fe381b11514c9af8cd4a5d571f3f3
notwitee <marchel.code@gmail.com>
2025-10-04 11:40:08 +0700

    refact: remove all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
-import { UserComponent } from './components/user/user.component';
-import { DUMMY_USERS } from './data/dummy-users';
-import { TasksComponent } from "./components/tasks/tasks.component";
+import { Component } from '@angular/core';
+
 @Component({
   standalone: true,
-    selector: 'app-root',
-    imports: [HeaderComponent, UserComponent, TasksComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  public users = DUMMY_USERS;
-  public selectedUserId = signal<string>('');
-
-  onSelectedUserId(id: string) {
-    this.selectedUserId.set(id);
-  }
-
-  get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId());
-  }
-}
+export class AppComponent {}
